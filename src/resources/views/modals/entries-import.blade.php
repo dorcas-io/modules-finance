4f9a399aaf99878af5495c3590981ac8f3d8a2ac
Olawuyi <olawuyi@Olawuyis-MacBook-Pro.local>
2022-07-22 19:14:36 +0100

    some fixes

--- src/resources/views/modals/entries-import.blade.php
+++ src/resources/views/modals/entries-import.blade.php
@@ -2,7 +2,7 @@
 	<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
-				<h5 class="modal-title" id="entries-import-modalLabel">{{ $importEntriesModal or 'Import Account Entries' }}</h5>
+				<h5 class="modal-title" id="entries-import-modalLabel">{{ $importEntriesModal ?? 'Import Account Entries' }}</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"></button>
 			</div>
 			<div class="modal-body">
